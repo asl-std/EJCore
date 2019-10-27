9f8afa54f6a21457ef43e173156e1a284194f81b
Snoop1CattZ69 <hor1999@mail.ru>
2019-10-27 16:13:32 +0200

    Some changes with EJP, added way to load/reload cfgs and listeners
    Signed-off-by: Snoop1CattZ69 <hor1999@mail.ru>

--- src/ru/asl/api/bukkit/plugin/EJPlugin.java
+++ src/ru/asl/api/bukkit/plugin/EJPlugin.java
@@ -1,8 +1,17 @@
 package ru.asl.api.bukkit.plugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import ru.asl.api.ejcore.yaml.EJConf;
+
 public abstract class EJPlugin extends JavaPlugin {
+
+	private List<EJConf> cfgs = new ArrayList<>();
 
 	@Override
 	public void onEnable() {
@@ -11,12 +20,15 @@ public abstract class EJPlugin extends JavaPlugin {
 		preInit();
 
 		init();
+
+		reloadConfigurations();
 	}
 
 	@Override
 	public void onDisable() {
 		super.onDisable();
 
+		HandlerList.unregisterAll(this);
 		disabling();
 	}
 
@@ -25,5 +37,20 @@ public abstract class EJPlugin extends JavaPlugin {
 	public void preInit() {}
 
 	public void disabling() {}
+
+	public void loadConfigurations(EJConf... cfgs) { for (EJConf cfg : cfgs) loadConfiguration(cfg); }
+	public void loadListener(Listener listener) {getServer().getPluginManager().registerEvents(listener, this);}
+
+	public void loadConfiguration(EJConf cfg) {
+		if (cfgs.contains(cfg)) return;
+		else cfgs.add(cfg);
+		cfg.reload();
+	}
+
+	public void reloadConfigurations() {
+		for (EJConf cfg : cfgs)
+			cfg.reload();
+	}
+
 
 }
