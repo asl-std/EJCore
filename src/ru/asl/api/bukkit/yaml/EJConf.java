c21771c1e15360c2436459cfe6ed570361f68f53
Snoop1CattZ69 <hor1999@mail.ru>
2019-10-27 11:27:59 +0200

    Added abstract class with auto-extracting for YAML files

---
+++ src/ru/asl/api/bukkit/yaml/EJConf.java
@@ -0,0 +1,36 @@
+package ru.asl.api.bukkit.yaml;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+
+import ru.asl.api.bukkit.plugin.EJPlugin;
+
+public abstract class EJConf extends YAML {
+
+	public EJConf(File file, EJPlugin plugin) {
+		super(file);
+		if (!fileExists()) plugin.saveResource(file.getName(), false);
+
+		try {
+			load();
+		} catch (IOException | InvalidConfigurationException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		loadConfig();
+	}
+
+	public EJConf(String path, EJPlugin plugin) {
+		this(new File(path), plugin);
+	}
+
+	public abstract void loadConfig();
+
+	public void reload(){
+		super.reload();
+		loadConfig();
+	}
+}
