a9c08cc6a4843e6d0780df84aff1531166e42b4c
Snoop1CattZ69 <agronaut8323@gmail.com>
2022-05-15 14:59:35 +0300

    renamed #BlockUtil.getBlockInRadius into getBlocksCuboid added #BlockUtil.getBlocksFlat

--- src/ru/asl/api/ejcore/blocks/BlockUtil.java
+++ src/ru/asl/api/ejcore/blocks/BlockUtil.java
@@ -9,7 +9,7 @@ import ru.asl.api.bukkit.location.Vector3D;
 
 public class BlockUtil {
 
-	public static List<Block> getBlocksInRadius(Block target, int radius) {
+	public static List<Block> getBlocksCuboid(Block target, int radius) {
 		final List<Block> blocks = new ArrayList<>();
 
 		final Vector3D center = Vector3D.fromLocation(target.getLocation());
@@ -26,6 +26,25 @@ public class BlockUtil {
 					blocks.add(center.getWorld().getBlockAt(center.add(new Vector3D(x,y,z)).toLocation()));
 
 		return blocks;
+	}
+
+	public static List<Block> getBlocksFlat(Block target, int radius) {
+		final List<Block> blocks = new ArrayList<>();
+
+		final Vector3D center = Vector3D.fromLocation(target.getLocation());
+
+		if (radius%2 == 0)
+			radius-=1;
+
+		if (radius > 1)
+			radius /= 2;
+
+		for (int x = -radius ; x < radius ; x++)
+			for (int z = -radius ; z < radius ; z++)
+				blocks.add(center.getWorld().getBlockAt(center.add(new Vector3D(x,0,z)).toLocation()));
+
+		return blocks;
+
 	}
 
 }
