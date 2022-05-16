9c28a8b456b02283b2b15835b27ae8ced1ece72c
Snoop1CattZ69 <agronaut8323@gmail.com>
2022-05-16 17:34:15 +0300

    changed BlockUtil methods block collecting

--- src/ru/asl/api/ejcore/blocks/BlockUtil.java
+++ src/ru/asl/api/ejcore/blocks/BlockUtil.java
@@ -12,18 +12,16 @@ public class BlockUtil {
 	public static List<Block> getBlocksCuboid(Block target, int radius) {
 		final List<Block> blocks = new ArrayList<>();
 
-		final Vector3D center = Vector3D.fromLocation(target.getLocation());
-
 		if (radius%2 == 0)
 			radius-=1;
 
 		if (radius > 1)
 			radius /= 2;
 
-		for (int x = -radius ; x < radius ; x++)
-			for (int y = -radius ; y < radius ; y++)
-				for (int z = -radius ; z < radius ; z++)
-					blocks.add(target.getWorld().getBlockAt(center.add(new Vector3D(x,y,z)).toLocation()));
+		for (int x = -radius ; x < radius+1 ; x++)
+			for (int y = -radius ; y < radius+1 ; y++)
+				for (int z = -radius ; z < radius+1 ; z++)
+					blocks.add(target.getWorld().getBlockAt(new Vector3D(x,y,z).addTo(target.getLocation())));
 
 		return blocks;
 	}
@@ -31,17 +29,15 @@ public class BlockUtil {
 	public static List<Block> getBlocksFlat(Block target, int radius) {
 		final List<Block> blocks = new ArrayList<>();
 
-		final Vector3D center = Vector3D.fromLocation(target.getLocation());
-
 		if (radius%2 == 0)
 			radius-=1;
 
 		if (radius > 1)
 			radius /= 2;
 
-		for (int x = -radius ; x < radius ; x++)
-			for (int z = -radius ; z < radius ; z++)
-				blocks.add(target.getWorld().getBlockAt(center.add(new Vector3D(x,0,z)).toLocation()));
+		for (int x = -radius ; x < radius+1 ; x++)
+			for (int z = -radius ; z < radius+1 ; z++)
+				blocks.add(target.getWorld().getBlockAt(new Vector3D(x,0,z).addTo(target.getLocation())));
 
 		return blocks;
 
